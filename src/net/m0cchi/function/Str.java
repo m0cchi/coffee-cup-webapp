10d03a42a465ae961255e3c236e0ba23886eaf3c
m0cchi <boom.boom.planet@gmail.com>
2016-08-11 18:44:00 +0900

    impl str

---
+++ src/net/m0cchi/function/Str.java
@@ -0,0 +1,30 @@
+package net.m0cchi.function;
+
+import net.m0cchi.value.AtomicType;
+import net.m0cchi.value.Element;
+import net.m0cchi.value.Environment;
+import net.m0cchi.value.Function;
+import net.m0cchi.value.SList;
+import net.m0cchi.value.Value;
+
+public class Str extends Function {
+	private static final long serialVersionUID = 1252108221339033107L;
+
+	public Str() {
+		setArgs(new String[] { REST, "str args" });
+	}
+
+	@Override
+	public Element invoke(Environment environment) {
+		SList list = (SList) environment.getValue(getArgs()[1]);
+		Element[] values = list.toArray();
+		StringBuilder sb = new StringBuilder();
+		for (Element value : values) {
+			sb.append(((Value<?>) value).getNativeValue());
+		}
+
+		Value<String> ret = new Value<>(AtomicType.LETTER, sb.toString());
+		return ret;
+	}
+
+}
