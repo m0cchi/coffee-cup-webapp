826b6baa0e05ee2fcb1d6e262155c5fcce9221c2
m0cchi <boom.boom.planet@gmail.com>
2016-08-15 23:15:01 +0900

    add safe eval sample

--- src/net/m0cchi/startup/Bootstrap.java
+++ src/net/m0cchi/startup/Bootstrap.java
@@ -2,19 +2,29 @@ package net.m0cchi.startup;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 import net.m0cchi.function.DefRoutes;
 import net.m0cchi.function.Defun;
 import net.m0cchi.function.Defvar;
 import net.m0cchi.function.DoList;
+import net.m0cchi.function.Eval;
 import net.m0cchi.function.HttpService;
+import net.m0cchi.function.Nop;
 import net.m0cchi.function.Path2Stream;
 import net.m0cchi.function.Println;
 import net.m0cchi.function.ReadStream;
 import net.m0cchi.function.Str;
 import net.m0cchi.function.Template;
+import net.m0cchi.function.java.Invoke;
+import net.m0cchi.function.java.New;
 import net.m0cchi.util.Program;
+import net.m0cchi.value.AtomicType;
+import net.m0cchi.value.Element;
 import net.m0cchi.value.Environment;
+import net.m0cchi.value.Function;
+import net.m0cchi.value.SList;
+import net.m0cchi.value.Value;
 
 public class Bootstrap {
 
@@ -32,6 +42,82 @@ public class Bootstrap {
 		environment.defineFunction("read", new ReadStream());
 		environment.defineFunction("template", new Template());
 		environment.defineFunction("str", new Str());
+		environment.defineFunction(".", new Invoke());
+		environment.defineFunction("new", new New());
+		// safe eval sample
+		Nop nop = new Nop();
+		Eval eval = new Eval();
+		// easy
+		// eval.setRemoveAllFunction(true);
+		Arrays.asList(
+				new String[] {
+						"http-service",
+						"defvar",
+						"defun",
+						"defroutes",
+						"file",
+						"read",
+						"template",
+						".",
+						"new",
+						"eval",
+						"str",
+						"println",
+						"do"
+		}).stream().forEach(str -> eval.hook(str, nop));
+		
+		String message = "code runner<br>"
+				+ "/code-runner?code=(greeting-message) ;;=> this message<br>"
+				+ "/code-runner?code=message ;;=> this message<br>"
+				+ "<a href=\"/code-runner?code=(+ 1 2)\">/code-runner?code=(+ 1 2) ;;=> ???</a>";
+		eval.hook("message", new Value<String>(AtomicType.LETTER, message));
+		eval.hook("greeting-message", new Function() {
+			private static final long serialVersionUID = 8467543795727484536L;
+
+			@Override
+			public Element invoke(Environment environment) {
+				return environment.getValue("message");
+			}
+		});
+		eval.hook("+", new Function() {
+			private static final long serialVersionUID = -6114148177013110076L;
+			{
+				setArgs("number 1", "number 2");
+			}
+
+			@Override
+			public Element invoke(Environment environment) {
+				Element num1Value = environment.getValue(getArgs()[0]);
+				Element num2Value = environment.getValue(getArgs()[1]);
+				Element ret = new SList();
+				if (!(num1Value instanceof Value && num2Value instanceof Value)) {
+					// error
+					return ret;
+				}
+				Object obj1 = ((Value<?>) num1Value).getNativeValue();
+				Object obj2 = ((Value<?>) num2Value).getNativeValue();
+				Integer num1;
+				Integer num2;
+				if (obj1 instanceof Integer) {
+					num1 = (Integer) obj1;
+				} else if (obj1 instanceof Double) {
+					num1 = ((Double) obj1).intValue();
+				} else {
+					return ret;
+				}
+
+				if (obj2 instanceof Integer) {
+					num2 = (Integer) obj2;
+				} else if (obj2 instanceof Double) {
+					num2 = ((Double) obj2).intValue();
+				} else {
+					return ret;
+				}
+				return new Value<Integer>(AtomicType.DIGIT, num1 + num2);
+			}
+		});
+
+		environment.defineFunction("eval", eval);
 		program.run();
 	}
 
