c5f4ad990af7397b9e08481fb31c5d074c667397
m0cchi <boom.boom.planet@gmail.com>
2016-08-25 22:32:43 +0900

    impl setq

---
+++ src/net/m0cchi/function/Setq.java
@@ -0,0 +1,30 @@
+package net.m0cchi.function;
+
+import net.m0cchi.parser.semantic.SemanticAnalyzer;
+import net.m0cchi.value.Element;
+import net.m0cchi.value.Environment;
+import net.m0cchi.value.Macro;
+import net.m0cchi.value.NULL.NIL;
+import net.m0cchi.value.Value;
+
+public class Setq extends Macro {
+	private static final long serialVersionUID = -2553177194125121833L;
+
+	public Setq() {
+		setArgs(new String[] { "def name", "def value" });
+	}
+
+	@Override
+	public Element invoke(Environment environment) {
+		@SuppressWarnings("unchecked")
+		Value<String> name = (Value<String>) environment.getValue(getArgs()[0]);
+		@SuppressWarnings("unchecked")
+		Value<String> value = (Value<String>) environment.getValue(getArgs()[1]);
+		Environment parent = environment.getParent();
+		SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer(environment);
+		parent.setValue(name.getNativeValue(), semanticAnalyzer.evaluate(value));
+
+		return NIL.NIL;
+	}
+
+}
